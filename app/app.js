96781e36ce3096188a81ca3f25fe70f1eab212c5
komalmabbu <komalmabbu@gmail.com>
2026-04-12 11:31:03 +0000

    initial commit

---
+++ app/app.js
@@ -0,0 +1,9 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from DevOps Project!');
+});
+
+app.listen(3000, () => console.log('Running on port 3000'));
